01e07924ccdc4d1dd854eb798875490545a31071
Aditya Kumar <97836516+adityak54@users.noreply.github.com>
2024-06-28 16:26:49 +0530

    updated backend URL

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/api/",
+  baseURL: "https://mern-social-backend-ten.vercel.app/api",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
